1be8f3afce91ef6519b6dd0b2350ad05eed472ed
scottstanton <scottstanton@scottstanton.us>
2021-01-18 20:58:38 -0500

    Added arguments with argparse

--- ticker.py
+++ ticker.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 # 
+import argparse
 import json
-import pprint
-import re
 import requests
 import sys
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s','--stock','--stocks', required=True, nargs='*',
+       metavar='stock_symbol', help="List of stocks to get information for.")
+parser.add_argument('-p','--params', nargs='*', metavar='known_parameters',
+       help="Optional list of parameters instead of the usual output. **Not yet imple                                                                                        mented**")
+parser.add_argument('-b','--boundary', nargs=2, type=int,
+       help="Use to find out if the stock has reached the lower or upper boundary.")
+
+args = parser.parse_args()
+
+if args.params and args.boundary:
+    parser.error("--params and --boundary cannot be used together.")
+
+
 
 # Let's setup some functions for later use
 
@@ -33,13 +48,13 @@ def printStockData():
    else:
       upDown = 'unchanged at'
 
-   # marketState can be PRE, REGULAR, POST or POSTPOST
+   # marketState can be PRE, REGULAR, POST or POSTPOST or CLOSED
    if stockData.get('marketState','err') == 'PRE':
-      txt = "{} closed yessterday at {:.2f} {} {:.2f}. Yesterday's range was {}."
+      txt = "{} closed yessterday at {:.2f} {} {:.2f}%. Yesterday's range was {}."
    elif stockData.get('marketState','err') == 'REGULAR':
-      txt = "{} is currently at {:.2f} {} {:.2f}. The day's range so far is {}."
+      txt = "{} is currently at {:.2f} {} {:.2f}%. The day's range so far is {}."
    else:
-      txt = "{} closed at {:.2f} {} {:.2f}. The day's range is {}."
+      txt = "{} closed at {:.2f} {} {:.2f}%. The day's range is {}."
 
    print(txt.format(stockData.get('symbol','err'),stockData.get('regularMarketPrice',0),upDown,stockData.get('regularMarketChangePercent',0),stockData.get('regularMarketDayRange','err')))
 # End of printStockData
@@ -47,19 +62,20 @@ def printStockData():
 
 # Main program
 # Deal with the command line options
-argList=sys.argv
-del argList[0]
-
-if len(argList) == 0:
-   print('You need to supply at least one stock symbol.')
-   sys.exit(99)
 
 # Run through each stock symbol listed on the command line
-for stocks in argList:
-   getStockData(stocks)
-   # print(list(stockData.keys()))
-   # print(list(stockData.values()))
-   # print('State is ' + stockData.get('marketState','err')  + '  eol')
-   printStockData()
+if args.params:
+   #print params
+   sys.exit(99)
+elif args.boundary:
+   #Do boundary stuff
+   sys.exit(99)
+else:
+   for stocks in args.stock:
+      getStockData(stocks)
+      # print(list(stockData.keys()))
+      # print(list(stockData.values()))
+      # print('State is ' + stockData.get('marketState','err')  + '  eol')
+      printStockData()
 
 #End of program
